b0e729f2584fcfb55a2dc673ce11c622d2e7ecc8
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-17 06:07:02 +0100

    started using styled components for my css styling

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import styled from "styled-components"
 
 export const LandingPage = () => {
   return (
-    <main>
+    <Wrapper>
         <nav>
             <img src={""} alt = "jobster logo" className='logo'/>
         </nav>
@@ -12,11 +13,39 @@ export const LandingPage = () => {
 
           <div>
            <h1>Jobs <span>Tracking</span> App</h1>
-           <p>lorem10</p>
+           <p>lorem go yo cd ngndfgg  sjjdj   nvxcvj jdjfn ujdfoho IDBLSDD fddnslaskfkfjjk jfdjjskkn</p>
+           <button className='btn btn-hero'>Signin / Login </button>
           </div>
-
-          <img src={''} alt= "logo" className='img main-img'/>
+{/* 
+          <img src={''} alt= "logo" className='img main-img'/> */}
+          <p>loeoff dfepfprmf weohoehho ereildn</p>
          </div>  
-    </main>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.main `
+nav {
+  width : var(--fluid-width);
+  max-width: var(--max-width);
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  height: var(--nav-height);
+}
+
+.page {
+  min-height: calc(100vh - var(--nav-height));
+  display: grid;
+  align-items: center;
+  margin-top: -3rem;
+}
+
+h1 {
+  font-weight: 700;
+
+  span {
+    color : var(--primary-500)
+  }
+}
+`
