cdc7a6f111025925bc58517c1b0aaebf316785f1
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-17 16:43:53 +0100

    Done with Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import error from "../assets/images/error.svg" 
+import Wrapper from '../assets/wrappers/ErrorPage'
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <Wrapper className='full-page'>
+     <div>
+      <img src={error} alt='not-found'/>
+      <h3>Ohh!! Page not found</h3>
+      <p>Sorry, the page you are trying to access doen't exist</p>
+      <Link to= {"/"} className = "btn" style={{color : "white", textDecoration : "none"}}>Go Home</Link>
+     </div>
+    </Wrapper>
   )
 }
 
