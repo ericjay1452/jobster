27af6dd9e0cfe220d4a3538f7956138b6fa59146
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-17 15:19:02 +0100

    committed my codes

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,69 +1,78 @@
-import React from 'react'
-import styled from "styled-components"
+import React from 'react';
+import styled from 'styled-components';
+import people from '../assets/images/people.svg';
 
 export const LandingPage = () => {
-  return (
-    <Wrapper>
-        <nav>
-            <img src={""} alt = "jobster logo" className='logo'/>
-        </nav>
+	return (
+		<Wrapper>
+			<nav>
+				<img src={''} alt="jobster logo" className="logo" />
+			</nav>
 
-        <div className='container page'>
-          {/* Two column layout */}
+			<div className="container page">
+				{/* Two column layout */}
+				<div>
+					<h1>
+						Jobs <span>Tracking</span> App
+					</h1>
+					<p>
+						I'm baby vice williamsburg thundercats, 8-bit you probably haven't heard of them chillwave
+						organic food truck banh mi. Cray succulents tilde, four dollar toast health goth lyft snackwave
+						tote bag retro. Edison bulb master cleanse meditation ethical. Tousled etsy selfies freegan la
+						croix sustainable. Direct trade tousled austin chicharrones before they sold out yes plz
+						slow-carb 8-bit hell of master cleanse. Unicorn schlitz tumblr poutine, vinyl cloud bread
+						post-ironic normcore bicycle rights chicharrones echo park bruh kombucha literally.
+					</p>
+					<button className="btn btn-hero">Signin / Login </button>
+				</div>
 
-          <div>
-           <h1>Jobs <span>Tracking</span> App</h1>
-           <p>lorem go yo cd ngndfgg  sjjdj   nvxcvj jdjfn ujdfoho IDBLSDD fddnslaskfkfjjk jfdjjskkn</p>
-           <button className='btn btn-hero'>Signin / Login </button>
-          </div>
+				<img src={people} alt="logo" className="img main-img" />
+			</div>
+		</Wrapper>
+	);
+};
 
-          <img src={''} alt= "logo" className='img main-img'/>
-         </div>  
-    </Wrapper>
-  )
-}
+const Wrapper = styled.main`
+	nav {
+		width: var(--fluid-width);
+		max-width: var(--max-width);
+		margin: 0 auto;
+		display: flex;
+		align-items: center;
+		height: var(--nav-height);
+	}
 
-const Wrapper = styled.main `
-nav {
-  width : var(--fluid-width);
-  max-width: var(--max-width);
-  margin: 0 auto;
-  display: flex;
-  align-items: center;
-  height: var(--nav-height);
-}
+	.page {
+		min-height: calc(100vh - var(--nav-height));
+		display: grid;
+		align-items: center;
+		margin-top: -3rem;
+	}
 
-.page {
-  min-height: calc(100vh - var(--nav-height));
-  display: grid;
-  align-items: center;
-  margin-top: -3rem;
-}
+	h1 {
+		font-weight: 700;
 
-h1 {
-  font-weight: 700;
+		span {
+			color: var(--primary-500);
+		}
+	}
 
-  span {
-    color : var(--primary-500)
-  }
-}
+	p {
+		color: var(--grey-600);
+	}
 
-p {
-  color: var(--grey-600);
-}
+	.main-img {
+		display: none;
+	}
 
-.main-img {
-  display: none;
-}
+	@media (min-width: 992px) {
+		.page {
+			grid-template-columns: 1fr 1fr;
+			column-gap: 3rem;
+		}
 
-@media (min-width: 992px) {
-  .page {
-    grid-template-columns: 1fr 1fr;
-    column-gap: 3rem;
-  }
-
-  .main-img {
-    display: block;
-  }
-}
-`
+		.main-img {
+			display: block;
+		}
+	}
+`;
