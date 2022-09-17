87247e6c48d455ac48a9c53bddd3f45f33cad818
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-17 01:48:39 +0100

    continue tomorow

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,6 +2,21 @@ import React from 'react'
 
 export const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <main>
+        <nav>
+            <img src={""} alt = "jobster logo" className='logo'/>
+        </nav>
+
+        <div className='container page'>
+          {/* Two column layout */}
+
+          <div>
+           <h1>Jobs <span>Tracking</span> App</h1>
+           <p>lorem10</p>
+          </div>
+
+          <img src={''} alt= "logo" className='img main-img'/>
+         </div>  
+    </main>
   )
 }
