67b10d9b97700fc63d0ac635eee3e60fd5821c3a
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-18 22:40:33 +0100

    styling my sharelayout

--- src/pages/Dashoard/SharedLayout.js
+++ src/pages/Dashoard/SharedLayout.js
@@ -1,8 +1,25 @@
 import React from 'react'
+// always import Outlet from react-router-dom for accessing sharedlayout
+import { Outlet } from 'react-router-dom'
+import { BigSideBar,SmallSideBar, Navbar } from '../../components'
+import Wrapper from '../../assets/wrappers/SharedLayout'
 
 const SharedLayout = () => {
   return (
-    <div>SharedLayout</div>
+    <Wrapper>
+      <main className='dashboard'>
+        <SmallSideBar />
+        <BigSideBar />
+
+        <div>
+          <Navbar />
+          <div className='dashboard-page'>
+            {/* Display your Outlet here */}
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
   )
 }
 
