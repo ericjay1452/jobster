b0a36453af1efe87cefad47d86f55ab121eec153
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-18 22:01:46 +0100

    created my sharedlayout for dashboard

---
+++ src/pages/Dashoard/index.js
@@ -0,0 +1,4 @@
+export {default as AddJobs} from "../Dashoard/AddJobs"
+export {default as AllJobs} from "../Dashoard/AllJobs"
+export {default as Profile} from "../Dashoard/Profile"
+export {default as Stats} from "../Dashoard/Stats"
