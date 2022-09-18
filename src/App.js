3c9d19e94de1a91a1d90c73d04bfbd6faac67116
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-18 22:15:18 +0100

    created my sharedlayout routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react'
 import {Route, Routes} from "react-router-dom"
 import { ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css"
-import { Landing, Dashboard, Error, Register, BrowserRouter} from './pages'
+import { Landing, Error, Register} from './pages'
+import {AddJobs, AllJobs, Profile, Stats, SharedLayout } from "./pages/Dashoard" 
 
  const App = () => {
   return (
     <>
     <Routes>
-      <Route path='/' exact element = {<Dashboard />} />
+      <Route path='/' exact element = {<SharedLayout />}>
+        <Route index element = {<Profile/> }/>
+       <Route element = {<Stats />} path="stats"/>
+       <Route element = {<AllJobs />} path="all-jobs"/>
+       <Route element = {<AddJobs />} path="add-jobs"/>
+      </Route>
       <Route path='register' element = {<Register />} />
       <Route path='landing' element = {<Landing />} />
       <Route path='*' exact element = {<Error />} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
 export {default as Error} from "../pages/Error"
-export {default as Dashboard} from "../pages/Dashboard"
 export {default as Register} from "../pages/Register"
 export {default as Landing} from "../pages/Landing"
